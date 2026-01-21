eb6e2ad21faaa89c594b9d8b259a9f4d216149aa
Danil <danilprokopyshen2007@gmail.com>
2026-01-21 12:56:17 +0100

    whatever

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,22 +1,18 @@
-import { useState } from "react"
-
 function Task({ id, title, desc, completed }) {
 
     const taskDone = completed ? "text-decoration-line-through" : ""
 
     return(
-        <>
-            <div className="card text-bg-light mb-3" style={{maxWidth: "18rem"}}>
-                <div className={`card-header ${taskDone}`}>
-                    {title}
-                </div>
-                <div className="card-body">
-                    <p className="card-text">
-                        {desc}
-                    </p>
-                </div>
+        <div className="card text-bg-light mb-3" style={{maxWidth: "18rem"}}>
+            <div className={`card-header ${taskDone}`}>
+                {title}
             </div>
-        </>
+            <div className="card-body">
+                <p className="card-text">
+                    {desc}
+                </p>
+            </div>
+        </div>
     )
 }
 
